f181308e041b73f1b6044ece08284f28cf2f3b0d
Unionkele <871917113@qq.com>
2019-06-25 17:42:05 +0800

    登录

--- module/Login/LoginForm.Designer.cs
+++ module/Login/LoginForm.Designer.cs
@@ -29,96 +29,126 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.tbLoginName = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tbLoginPwd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbLoginer = new System.Windows.Forms.ComboBox();
+            this.subLogin = new System.Windows.Forms.Button();
+            this.res = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(146, 29);
+            this.label1.Location = new System.Drawing.Point(59, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(193, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "图书管理系统";
+            this.label1.Text = "图书信息系统";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbLoginer);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tbLoginPwd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbLoginName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(281, 138);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(80, 124);
+            this.label2.Location = new System.Drawing.Point(49, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 19);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "用户登录：";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "用户名";
+            // 
+            // tbLoginName
+            // 
+            this.tbLoginName.Location = new System.Drawing.Point(110, 32);
+            this.tbLoginName.Name = "tbLoginName";
+            this.tbLoginName.Size = new System.Drawing.Size(121, 21);
+            this.tbLoginName.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(118, 207);
+            this.label3.Location = new System.Drawing.Point(61, 72);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 19);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "密码：";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "密码";
             // 
-            // textBox1
+            // tbLoginPwd
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBox1.Location = new System.Drawing.Point(215, 122);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(142, 21);
-            this.textBox1.TabIndex = 0;
+            this.tbLoginPwd.Location = new System.Drawing.Point(110, 69);
+            this.tbLoginPwd.Name = "tbLoginPwd";
+            this.tbLoginPwd.Size = new System.Drawing.Size(121, 21);
+            this.tbLoginPwd.TabIndex = 1;
             // 
-            // textBox2
+            // label4
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBox2.Location = new System.Drawing.Point(215, 205);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(142, 21);
-            this.textBox2.TabIndex = 2;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "登录者";
             // 
-            // button1
+            // cbLoginer
             // 
-            this.button1.Location = new System.Drawing.Point(133, 275);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "登录";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.cbLoginer.FormattingEnabled = true;
+            this.cbLoginer.Items.AddRange(new object[] {
+            "普通用户",
+            "管理者"});
+            this.cbLoginer.Location = new System.Drawing.Point(110, 103);
+            this.cbLoginer.Name = "cbLoginer";
+            this.cbLoginer.Size = new System.Drawing.Size(121, 20);
+            this.cbLoginer.TabIndex = 3;
             // 
-            // button2
+            // subLogin
             // 
-            this.button2.Location = new System.Drawing.Point(264, 275);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "注册";
-            this.button2.UseVisualStyleBackColor = true;
+            this.subLogin.Location = new System.Drawing.Point(63, 211);
+            this.subLogin.Name = "subLogin";
+            this.subLogin.Size = new System.Drawing.Size(75, 23);
+            this.subLogin.TabIndex = 2;
+            this.subLogin.Text = "登录";
+            this.subLogin.UseVisualStyleBackColor = true;
+            this.subLogin.Click += new System.EventHandler(this.subLogin_Click);
+            // 
+            // res
+            // 
+            this.res.Location = new System.Drawing.Point(177, 211);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(75, 23);
+            this.res.TabIndex = 3;
+            this.res.Text = "注册";
+            this.res.UseVisualStyleBackColor = true;
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(486, 385);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(305, 258);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.subLogin);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Name = "LoginForm";
             this.Text = "登录界面";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,11 +157,14 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbLoginer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbLoginPwd;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbLoginName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button subLogin;
+        private System.Windows.Forms.Button res;
     }
 }
--- module/Login/LoginForm.cs
+++ module/Login/LoginForm.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using NetDB.Core;
+using NetDB.Core.Support;
+using BookManager.model;
+
 namespace BookManager.module.Login
 {
     public partial class LoginForm : Form
@@ -16,20 +20,75 @@ namespace BookManager.module.Login
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void LoginCheck(string identity)
         {
-            if (textBox1.Text != "" && textBox2.Text != "")
+            switch (identity)
             {
-                string username = textBox1.Text;
-                string password = textBox2.Text;
-                string seek = "select count(*) from Person where PersonName=@username";
-            }
-            else {
-                MessageBox.Show("不能为空","提示");
-            }
-         
+                case "普通用户":
+                    if (tbLoginName.Text != "" && tbLoginPwd.Text != "")
+                    {
+                        Person person = new Person();
+                        person.PersonNum = tbLoginName.Text;                     
+                        bool res = person.Find();
+                        if (res)
+                        {
+                            person.PersonNum = tbLoginName.Text;  
+                            person.PersonCode = tbLoginPwd.Text; 
+                            person.Find();
+                            if (person.Find())
+                            {
+                                MessageBox.Show("登录成功","提示信息");
+                                this.Hide();
+                                new MainForm().Show();
+                            }
+                            else
+                            {
+                                MessageBox.Show("用户或密码错误","提示信息");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("用户不存在","提示信息");
+                        }
+                    }
+                    break;
+                case "管理者":
+                    if (tbLoginName.Text != "" && tbLoginPwd.Text != "")
+                    {
+                        model.Manager mLogin = new model.Manager();
+                        mLogin.ManagerName = tbLoginName.Text;
+                        bool res = mLogin.Find();
+                        if (res)
+                        {
+                            mLogin.ManagerName = tbLoginName.Text;
+                            mLogin.ManagerCode = tbLoginPwd.Text;
+                            mLogin.Find();
+                            if (mLogin.Find())
+                            {
+                                MessageBox.Show("登录成功", "提示信息");
+                                this.Hide();
+                                new Manager.BookManage.DisplayBook().Show();
+                            }
+                            else
+                            {
+                                MessageBox.Show("用户或密码错误", "提示信息");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("用户不存在", "提示信息");
+                        }
 
+                    }
+                    break;
+            }
         }
+
+        private void subLogin_Click(object sender, EventArgs e)
+        {
+            string LoginType = cbLoginer.Text;
+            LoginCheck(LoginType);
+        }
+
     }
 }
