f03d2b3ec3edf4a0b9f1896404c967f8f02f76cd
Huangyu123456 <51981623+Huangyu123456@users.noreply.github.com>
2019-06-26 12:11:15 +0800

    626

---
+++ module/book/TcInfo.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace BookManager.module.book
+{
+    partial class TcInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookWriter = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookPublish = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookRemark = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox1.Location = new System.Drawing.Point(59, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(672, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "查询";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(763, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.BookName,
+            this.BookWriter,
+            this.BookPublish,
+            this.BookDate,
+            this.BookPrice,
+            this.BookNum,
+            this.BookType,
+            this.BookRemark});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(835, 385);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.Visible = false;
+            // 
+            // BookName
+            // 
+            this.BookName.DataPropertyName = "BookName";
+            this.BookName.HeaderText = "书名";
+            this.BookName.Name = "BookName";
+            // 
+            // BookWriter
+            // 
+            this.BookWriter.DataPropertyName = "BookWriter";
+            this.BookWriter.HeaderText = "作者";
+            this.BookWriter.Name = "BookWriter";
+            // 
+            // BookPublish
+            // 
+            this.BookPublish.DataPropertyName = "BookPublish";
+            this.BookPublish.HeaderText = "出版社";
+            this.BookPublish.Name = "BookPublish";
+            // 
+            // BookDate
+            // 
+            this.BookDate.DataPropertyName = "BookDate";
+            this.BookDate.HeaderText = "出版时间";
+            this.BookDate.Name = "BookDate";
+            // 
+            // BookPrice
+            // 
+            this.BookPrice.DataPropertyName = "BookPrice";
+            this.BookPrice.HeaderText = "价格";
+            this.BookPrice.Name = "BookPrice";
+            // 
+            // BookNum
+            // 
+            this.BookNum.DataPropertyName = "BookNum";
+            this.BookNum.HeaderText = "数量";
+            this.BookNum.Name = "BookNum";
+            // 
+            // BookType
+            // 
+            this.BookType.DataPropertyName = "BookType";
+            this.BookType.HeaderText = "类型";
+            this.BookType.Name = "BookType";
+            // 
+            // BookRemark
+            // 
+            this.BookRemark.DataPropertyName = "BookRemark";
+            this.BookRemark.HeaderText = "备注";
+            this.BookRemark.Name = "BookRemark";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(289, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(174, 20);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // TcInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 446);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "TcInfo";
+            this.Text = "图书信息显示";
+            this.Load += new System.EventHandler(this.TcInfo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookWriter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookPublish;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookRemark;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ module/book/TcInfo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NetDB.Core;
+using NetDB.Core.Support;
+using BookManager.model;
+
+namespace BookManager.module.book
+{
+    public partial class TcInfo : Form
+    {
+        public TcInfo()
+        {
+            InitializeComponent();
+        }
+        //书籍信息列表显示
+        public void showBookInfo()
+        {
+            PageList<BookInfo> bookList = ORMSupport.PageSelect<BookInfo>()
+                .Select();
+            dataGridView1.DataSource = bookList.Rows;
+        }
+        //显示搜索结果(通过书籍名称或作者姓名查询)(分类查询)
+        private void searchInfo(string key, string typekey)
+        {
+            PageList<BookInfo> searchInfo = new PageList<BookInfo>()
+            .AddWhere("BookName", "like", "%" + key + "%")
+            .AddWhere("BookWriter", "like", "%" + key + "%")
+            .AddWhere("BookType", typekey)
+            .Select();
+            dataGridView1.DataSource = searchInfo.Rows;
+        }
+        //加载下拉框
+        private void TypeList()
+        {
+            PageList<BookType> typelist = ORMSupport.PageSelect<BookType>()
+                .Select();
+            foreach (BookType item in typelist.Rows)
+            {
+                comboBox1.Items.Add(item.BType);
+            }
+        }
+
+        private void TcInfo_Load(object sender, EventArgs e)
+        {
+            showBookInfo();
+            TypeList();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchKey = textBox1.Text;
+            string typeKey = comboBox1.Text;
+            searchInfo(searchKey, typeKey);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
